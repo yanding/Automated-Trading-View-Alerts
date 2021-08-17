524db4439701716e6c595bb5c1fffda6ad3c3932
Vineet Kumar <44500552+vineetkia@users.noreply.github.com>
2021-08-17 21:16:39 +0530

    Update EndPoint.py

--- EndPoint.py
+++ EndPoint.py
@@ -6,6 +6,6 @@ url = "http://20.204.87.110/webhook"
 headers = CaseInsensitiveDict()
 headers["Content-Type"] = "application/json"
 
-data = 'DETECTION JSON PAYLOAD'
-resp = requests.post(url, headers=headers, data=str(data))
+data = '{"key": "x", "message": "Negative Divergence Detected", "exchange": "BINANCE", "action": "SELL", "ticker": "ETHUSDTPERP", "volume": "12345.2", "price": "3167.8", "time": "UTC +530 TIME"}'
+resp = requests.post(url, headers=headers, data=data)
 print(resp.status_code)
